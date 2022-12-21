7e45c42dea4d4ca08f8d06bda59e520f25135db2
Maria <sheyko.mari@mail.ru>
2022-12-21 13:00:24 +0300

    feat: add routing and 404 - page

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,29 @@
 import "./index.css";
 import { createSorting , createGoodsCards } from './components/mainPage/index';
 import { base } from './components/goodsBase';
+import { create404 } from './components/error/404'
 
-window.onload = function() {
-    createSorting();
-    createGoodsCards(base.products);
-}
+const route = (event: Event) => {
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", (event.target as HTMLAnchorElement).href);
+    handleLocation();
+};
+
+const validPaths = ["", "/", "/cart", "/product"];
+const handleLocation = () => {
+    const path = window.location.pathname;
+
+    if (path === '/') {
+      createSorting();
+      createGoodsCards(base.products);
+    } else if (path === "/cart") {
+      // TODO: add function createCart
+    } else { 
+      create404();
+    }
+}; 
+
+window.onpopstate = handleLocation;
+(window as any).route = route;
+handleLocation();
