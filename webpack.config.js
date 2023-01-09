2742ef814b7334627d1945b904940da3bcb2fc33
Maria <sheyko.mari@mail.ru>
2023-01-10 01:25:41 +0300

    feat: add redirects

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const mode = process.env.NODE_ENV || 'development';
 
@@ -20,64 +20,68 @@ module.exports = {
         port: 3000,
         open: true,
         hot: true,
-        historyApiFallback: true
+        historyApiFallback: true,
     },
     entry: path.resolve(__dirname, 'src', 'index.ts'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+        publicPath: '/',
         filename: 'index.[contenthash].js',
-        assetModuleFilename: 'assets/[name][ext]'
+        assetModuleFilename: 'assets/[name][ext]',
     },
     resolve: {
-      extensions: ['.ts', '.js'],
+        extensions: ['.ts', '.js'],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src', 'index.html')
+            template: path.resolve(__dirname, 'src', 'index.html'),
         }),
         new MiniCssExtractPlugin({
             filename: '[name].[contenthash].css',
         }),
         new CopyPlugin({
-          patterns: [
-            {
-              from: '**/*',
-              context: path.resolve(__dirname, 'src', 'assets'),
-              to: './assets',
-              noErrorOnMissing: true,
-            },
-          ],
+            patterns: [
+                {
+                    from: '**/*',
+                    context: path.resolve(__dirname, 'src', 'assets'),
+                    to: './assets',
+                    noErrorOnMissing: true,
+                },
+            ],
         }),
     ],
     module: {
         rules: [
-          {
-            test: /\.html$/i,
-            loader: "html-loader",
-          },
-          { 
-            test: /.ts$/i, 
-            use: 'ts-loader',
-          },
-          {
-            test: /\.(svg|png|jpg)$/i, 
-            type: 'asset/resource'
-          },
-          {
-            test: /\.(c|sa|sc)ss$/i,
-            use: [devMode ? "style-loader" : MiniCssExtractPlugin.loader, "css-loader"],
-          },
-          {
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              }
-            }
-          }
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /.ts$/i,
+                use: 'ts-loader',
+            },
+            {
+                test: /\.(svg|png|jpg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(c|sa|sc)ss$/i,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
         ],
-      }
-}
+    },
+};
